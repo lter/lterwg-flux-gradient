807cb3e7a8fff08754306a0c257ead7a63dff0b9
njlyon0 <nicholasjlyon@gmail.com>
2023-10-18 16:22:47 -0700

    Working on debugging 'unzip only' function

--- R/unzip_neon.R
+++ R/unzip_neon.R
@@ -12,55 +12,42 @@
 #' @author Alexis Helgeson, Nick Lyon
 #' 
 #' 
-unzip_neon <- function(in_path = NULL, out_path = NULL){
+unzip_neon <- function(in_path = NULL, out_path = NULL, quiet = FALSE){
 
   # Identify names of downloaded ZIP files
   zip.files <- dir(path = in_path, pattern = ".zip")
-
-# Unzip those files!
-for(j in 1:length(zip.files)){
-  unzip(file.path(sitename, "filesToStack00200", zip.files[j]), exdir = in_path)
-}
-
-# Identify the .gz files created by unzipping
-gz.files <- dir(path = in_path, pattern = ".gz")
-
-# Process those as well!
-for(k in 1:length(gz.files)){
-  R.utils::gunzip(file.path(in_path, gz.files[k]), 
-                  destname = file.path(out_path,
-                                       gsub(pattern = ".gz", replacement = "", 
-                                            x = gz.files[k])), 
-                  remove = F, overwrite = T)
-}
-
-}
-
-
-
-downloadUnzipNEON <- function(sitecode, zip.dir, startdate, enddate){
-  #downloaded NEON eddy-co data as zip file
-  #this fcn is a rate limiter, downloads can be lengthy, quicker to download off website?
-  zipsByProduct(dpID="DP4.00200.001", sitecode,startdate, enddate,package="basic", check.size=F)
   
-  #unzip files
-  #I think the stackEddy fcn can be used here but currently does not list CH4 concentrations as an option
-  #test <- stackEddy(filepath = paste0(site.dir, "/filesToStack00200/"), level="dp04")
-  #setwd(zip.dir)
-  setwd(paste0(zip.dir, "/filesToStack00200"))
+  # Error out for no ZIP files found
+  if(length(zip.files) == 0)
+    stop("No ZIP files found at specified `in_path`")
   
-  zip.files <-list.files(pattern=".zip")
+  # Message ZIP success if `quiet` argument doesn't specify the opposite
+  if(quiet != TRUE){
+    message("Found ", length(zip.files), " ZIP files")
+  }
+  
+  # Unzip those files!
   for(j in 1:length(zip.files)){
-    print(j)
-    unzip(zip.files[j]) 
-  }
-  folders <- list.files(pattern = "KONZ.DP4.00200.001")
-  
-  for(j in 3:length(folders)){
-    gz.files <-list.files(path = file.path(folders[j]), pattern=".gz", full.names = TRUE)
-    print(j)
-    gunzip(gz.files, remove=FALSE)
+    unzip(file.path(in_path, zip.files[j]), exdir = in_path)
   }
   
-  return(print(paste0("NEON files for ", sitecode, " are downloaded and unzipped h5 files can be found at ", zip.dir)))
+  # Identify the .gz files created by unzipping
+  gz.files <- dir(path = in_path, pattern = ".gz")
+  
+  # Error out if no .gz files are found
+  if(length(gz.files) == 0)
+    stop("No .gz files found in specified ZIP files")
+  
+  # Message ZIP success if `quiet` argument doesn't specify the opposite
+  if(quiet != TRUE){
+    message("Found ", length(gz.files), " .gz files")
+  }
+  
+  # Process those as well!
+  for(k in 1:length(gz.files)){
+    R.utils::gunzip(file.path(in_path, gz.files[k]), 
+                    destname = file.path(out_path,
+                                         gsub(pattern = ".gz", replacement = "", 
+                                              x = gz.files[k])), 
+                    remove = F, overwrite = T) }
 }
