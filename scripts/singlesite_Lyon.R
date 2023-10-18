c9dbe81729ceaaeb448937c0fed32a9650524d9f
njlyon0 <nicholasjlyon@gmail.com>
2023-10-18 12:17:13 -0700

    Finished cannibalizing `contHF` content (for exploratory purposes!)

--- scripts/singlesite_Lyon.R
+++ scripts/singlesite_Lyon.R
@@ -29,7 +29,7 @@ dir.create(path = file.path("data"), showWarnings = F)
 dir.create(path = file.path(sitename), showWarnings = F)
 
 ## ------------------------------ ##
-      # Download Data ----
+        # Download Data ----
 ## ------------------------------ ##
 
 # Set start and end date for observations
@@ -59,12 +59,16 @@ for(k in 1:length(gz.files)){
   R.utils::gunzip(file.path(sitename, "filesToStack00200", gz.files[k]), 
                   destname = file.path(sitename,
                                        gsub(pattern = ".gz", replacement = "", 
-                                            x = gz.files[k])), remove = F)
+                                            x = gz.files[k])), 
+                  remove = F, overwrite = T)
 }
 
 ## ------------------------------ ##
-    # Calculate CH4 Fluxes ----
+  # Initial Processing Steps ----
 ## ------------------------------ ##
+
+# Identify temporal resolution
+temporal_res <- "09m"
 
 # Grab h5 files to be passed to SiteAttributes and SiteDF
 hd.files <- dir(path = file.path(sitename), pattern = "\\.h5$")
@@ -73,14 +77,112 @@ hd.files <- dir(path = file.path(sitename), pattern = "\\.h5$")
 attr <- data.frame(rhdf5::h5readAttributes(file.path(sitename, hd.files[1]),
                                            name = paste0("/", sitecode))) %>%
   # Pare down to only some needed columns
-  dplyr::select(DistZaxsLvlMeasTow, DistZaxsCnpy) %>%
+  # dplyr::select(DistZaxsLvlMeasTow, DistZaxsCnpy) %>%
   # Generate a site column
   dplyr::mutate(Site = sitecode, .before = dplyr::everything())
 
 # Check that out
 dplyr::glimpse(attr)
 
-
+# Grab actual data in .hd5 files
+# for(g in 1:length(hd.files)){
+for(g in 1){
+  
+  # Identify focal file
+  focal.hd <- hd.files[g]
+  
+  # Starting message
+  message("Processing begun for '", focal.hd, "'")
+  
+  # List the contents of hdf5 file and returns a df with file contents
+  focal.df <- rhdf5::h5ls(file = file.path(sitename, focal.hd),
+                          recursive = T, all = T, datasetinfo = T,
+                          index_type = rhdf5::h5default("H5_INDEX"),
+                          order = rhdf5::h5default("H5_ITER"), 
+                          s3 = F, s3credentials = NULL, native = F)
+  
+  # Figure out the 'group' name
+  focal.group <- paste0("/", sitecode, "/dp01/data/ch4Conc")
+  
+  # Identify the index values within that
+  focal.indices <- unique(focal.df[which(focal.df$group == focal.group), ]$name)
+  
+  # Pare down to only the codes beginning with 000s
+  focal.0s <- unique(stringr::str_subset(string = focal.indices, pattern = "000"))
+  
+  # And further pare down to the desired temporal resolution
+  focal.temporal <- unique(stringr::str_subset(string = focal.0s, pattern = temporal_res))
+  
+  # And rip out just the height number
+  height.number <- as.numeric(stringr::str_sub(string = focal.temporal, start = 6, end = 6))
+  
+  # Make some placeholder data objects for later use
+  df_CH4 <- data.frame()
+  df_CO2 <- data.frame()
+  df_H2O <- data.frame()
+  
+  # Now we'll loop across the height numbers within this dataset
+  for(position in 1:length(height.number)){
+    
+    # Progress message
+    message("Processing height ", position, " (of ", length(height.number), ")")
+    
+    # Set gas concentration dataset names
+    ch4.dir <- paste0("/", sitecode, '/dp01/data/ch4Conc/000_0', 
+                      height.number[position], "0_", temporal_res, "/rtioMoleDryCh4")
+    co2.dir <- paste0("/", sitecode, '/dp01/data/isoCo2/000_0', 
+                      height.number[position], "0_", temporal_res, "/rtioMoleDryCo2")
+    h2o.dir <- paste0("/", sitecode, '/dp01/data/isoH2o/000_0',
+                      height.number[position], "0_", temporal_res, "/rtioMoleDryH2o")
+    
+    # Set the QFQM dataset name to be used by h5read
+    ch4.qfqm <- paste0("/", sitecode, '/dp01/qfqm/ch4Conc/000_0', 
+                       height.number[position], "0_", temporal_res, "/rtioMoleDryCh4")
+    co2.qfqm <- paste0("/", sitecode, '/dp01/qfqm/isoCo2/000_0', 
+                       height.number[position], "0_", temporal_res, "/rtioMoleDryCo2")
+    h2o.qfqm <- paste0("/", sitecode, '/dp01/qfqm/isoH2o/000_0',
+                       height.number[position], "0_", temporal_res, "/rtioMoleDryH2o")
+    
+    # Read in h5 files as arrays
+    CH4 <- rhdf5::h5read(file = file.path(sitename, focal.hd), name = ch4.dir)
+    CO2 <- rhdf5::h5read(file = file.path(sitename, focal.hd), name = co2.dir)
+    H2O <- rhdf5::h5read(file = file.path(sitename, focal.hd), name = h2o.dir)
+    
+    # Read in QFQM files
+    CH4.qfqm <- rhdf5::h5read(file = file.path(sitename, focal.hd), name = ch4.qfqm)
+    CO2.qfqm <- rhdf5::h5read(file = file.path(sitename, focal.hd), name = co2.qfqm)
+    H2O.qfqm <- rhdf5::h5read(file = file.path(sitename, focal.hd), name = h2o.qfqm)
+    
+    # Combine data with QFQMs (and add tower position)
+    CH4_all <- dplyr::full_join(CH4, CH4.qfqm, by = c("timeBgn", "timeEnd")) %>%
+      dplyr::mutate(tower.position = height.number[position],
+                    .before = dplyr::everything())
+    CO2_all <- dplyr::full_join(CO2, CO2.qfqm, by = c("timeBgn", "timeEnd")) %>%
+      dplyr::mutate(tower.position = height.number[position],
+                    .before = dplyr::everything())
+    H2O_all <- dplyr::full_join(H2O, H2O.qfqm, by = c("timeBgn", "timeEnd")) %>%
+      dplyr::mutate(tower.position = height.number[position],
+                    .before = dplyr::everything())
+    
+    # Combine with prior iterations of the loop (and arrange by beginning time)
+    df_CH4 <- dplyr::bind_rows(df_CH4, CH4_all) %>%
+      dplyr::arrange(timeBgn)
+    df_CO2 <- dplyr::bind_rows(df_CO2, CO2_all) %>%
+      dplyr::arrange(timeBgn)
+    df_H2O <- dplyr::bind_rows(df_H2O, H2O_all) %>%
+      dplyr::arrange(timeBgn)
+    
+  } # Close tower position loop
+  
+  # Assemble that information into a list!
+  tower_conc = list(attr = attr, 
+                    CH4 = df_CH4, 
+                    CO2 = df_CO2, 
+                    H2O = df_H2O)
+  
+  
+  
+}
 
 
 # (vVv) BASEMENT (vVv) ----
