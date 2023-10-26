d0626c976f592e897c9149e203d5d479fb612b19
Helgeson <ah2595@ysey0wqr9w2hw.local>
2023-10-26 15:20:30 -0400

    "added provisional arg"

--- R/flow.downloadNEON.R
+++ R/flow.downloadNEON.R
@@ -4,14 +4,15 @@ library(neonUtilities)
 library(dplyr)
 #set start and end dates for data, REMEMBER CH4 is only available (august 2021 - present)
 startdate <- "2021-08"
-enddate <- "2021-09"
+enddate <- "2023-09"
 #set NEON site code
-sitecode <- 'GUAN'
+sitecode <- 'TOOL'
+#set include.provisional = T to get full time series of data up to present, currently provisional covers (2022-07:2023-09) 
 #grab relative humidity at 1 min resolution
 RH1min <- loadByProduct("DP1.00098.001", site=sitecode,
                     timeIndex=1, package="basic",
                     startdate=startdate, enddate=enddate,
-                    check.size=F)
+                    check.size=F, include.provisional = T)
 RH1min <- RH1min$RH_1min %>%
   filter(horizontalPosition == "000") %>%
   mutate(TowerPosition = as.numeric(verticalPosition)/10) %>%
@@ -20,7 +21,7 @@ RH1min <- RH1min$RH_1min %>%
 RH30min <- loadByProduct("DP1.00098.001", site=sitecode,
                         timeIndex=30, package="basic",
                         startdate=startdate, enddate=enddate,
-                        check.size=F)
+                        check.size=F, include.provisional = T)
 RH30min <- RH30min$RH_30min %>%
   filter(horizontalPosition == "000") %>%
   mutate(TowerPosition = as.numeric(verticalPosition)/10) %>%
@@ -29,7 +30,7 @@ RH30min <- RH30min$RH_30min %>%
 WS2D30min <- loadByProduct("DP1.00001.001", site=sitecode,
                       timeIndex=30, package="basic",
                       startdate=startdate, enddate=enddate,
-                      check.size=F)
+                      check.size=F, include.provisional = T)
 WS2D30min <- WS2D30min$twoDWSD_30min %>%
   mutate(TowerPosition = as.numeric(verticalPosition)/10) %>%
   select(TowerPosition, startDateTime, endDateTime, windSpeedMean, windSpeedFinalQF)
@@ -37,7 +38,7 @@ WS2D30min <- WS2D30min$twoDWSD_30min %>%
 WS2D2min <- loadByProduct("DP1.00001.001", site=sitecode,
                            timeIndex=2, package="basic",
                            startdate=startdate, enddate=enddate,
-                           check.size=F)
+                           check.size=F, include.provisional = T)
 WS2D2min <- WS2D2min$twoDWSD_2min %>%
   mutate(TowerPosition = as.numeric(verticalPosition)/10) %>%
   select(TowerPosition, startDateTime, endDateTime, windSpeedMean, windSpeedFinalQF)
@@ -48,4 +49,4 @@ dir.create(path = file.path("data"), showWarnings = F)
 dir.create(path = file.path("data", sitecode), showWarnings = F)
 save(DATA, file = paste0("data/", sitecode, "/", sitecode,"_NonEddyMetVars.Rdata"))
 #grab bundled eddy-covariance data
-zipsByProduct(dpID="DP4.00200.001", sitecode,startdate, enddate,package="basic", check.size=F, savepath = file.path("data", sitecode))
+zipsByProduct(dpID="DP4.00200.001", sitecode,startdate, enddate,package="basic", check.size=F, savepath = file.path("data", sitecode), include.provisional = T)
