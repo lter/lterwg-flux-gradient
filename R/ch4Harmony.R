700981fd2227721cbfc66c60e964fa56dab0b4ed
njlyon0 <nicholasjlyon@gmail.com>
2024-03-12 12:04:54 -0700

    Fleshed out data download step and drafted key skeleton creation

--- R/ch4Harmony.R
+++ R/ch4Harmony.R
@@ -18,8 +18,12 @@ librarian::shelf(tidyverse, googledrive, ltertools)
 # Clear environment
 rm(list = ls())
 
+# Create a local folder for data storage
+dir.create(path = file.path("methane"), showWarnings = F)
+dir.create(path = file.path("methane", "raw_methane"), showWarnings = F)
+
 ## ----------------------------- ##
-# Key Prep ----
+        # Data Download ----
 ## ----------------------------- ##
 
 # Identify desired files
@@ -28,16 +32,34 @@ ch4_files <- dplyr::bind_rows(
   googledrive::drive_ls(path = googledrive::as_id("https://drive.google.com/drive/u/0/folders/1qPrBaZxX7XBBKq77eEmVXALSoDmUB2_I")),
   googledrive::drive_ls(path = googledrive::as_id("https://drive.google.com/drive/u/0/folders/1AOct-UbwpzkuLMT9EnEspRX_QnX07T4G")) ) %>% 
   # Keep only CSVs / Excel files
-  dplyr::filter(stringr::str_detect(string = name, pattern = ".csv") == TRUE |
-                  stringr::str_detect(string = name, pattern = ".xls") == TRUE) %>% 
-  # Drop 'metadata'
-  dplyr::filter(stringr::str_detect(string = name, pattern = "metadata") != TRUE)
+  dplyr::filter(stringr::str_detect(string = name, pattern = ".csv") == T |
+                  stringr::str_detect(string = name, pattern = ".xls") == T) %>% 
+  # Drop non-data files that are the right file types
+  dplyr::filter(stringr::str_detect(string = name, pattern = "metadata") != T &
+                  stringr::str_detect(string = name, pattern = "data request") != T )
 
 # Check structure
-dplyr::glimpse(ch4_files)
-## tibble::view(ch4_files)
+ch4_files
 
 # Download desired files
+purrr::walk2(.x = ch4_files$id, .y = ch4_files$name,
+             .f = ~ googledrive::drive_download(file = .x, overwrite = T,
+                                                path = file.path("methane", 
+                                                                 "raw_methane", .y)))
+
+## ----------------------------- ##
+          # Key Prep ----
+## ----------------------------- ##
+# NOTE: This code is only run once to make the 'column key' skeleton
+## Which will be subsequently updated by hand
+
+# Generate key object
+ch4_key_v0 <- ltertools::begin_key(raw_folder = file.path("methane", "raw_methane"),
+                                   data_format = c("csv", "xlsx", "xls"),
+                                   guess_tidy = FALSE)
+
+
+
 
 
 ## ----------------------------- ##
