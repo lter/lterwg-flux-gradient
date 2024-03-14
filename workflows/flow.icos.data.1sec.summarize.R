f50c381f15ff698106f0e1c4a666d57afd69a655
njlyon0 <nicholasjlyon@gmail.com>
2024-03-14 15:40:34 -0700

    Fleshed out aggregation within 'half hour' chunks (within tower level)

--- workflows/flow.icos.data.1sec.summarize.R
+++ workflows/flow.icos.data.1sec.summarize.R
@@ -34,7 +34,7 @@ icos_v0 <- read.csv(file = file.path("data", "icos", "concentration_2020-001.csv
 
 # DELETE ME LATER ----
 ## Limit the number of rows to make it easier to develop this workflow
-icos_df <- icos_v0[1:1000, ]
+icos_df <- icos_v0[1:5000, ]
 
 # Check structure
 dplyr::glimpse(icos_df)
@@ -88,17 +88,40 @@ icos_v3 <- icos_v2 %>%
 # Check structure
 dplyr::glimpse(icos_v3)
 
+# Aggregate within 30-minute groups
+icos_v4 <- icos_v3 %>% 
+  # Identify 30-minute groups
+  dplyr::mutate(halfhour_group = ifelse(test = (gap == 0),
+                                        yes = 1, 
+                                        no = ceiling(gap / 1800))) %>% 
+  # Average within half hour group (also identify start/stop of half hour chunk)
+  dplyr::group_by(halfhour_group, LEVEL) %>% 
+  dplyr::summarize(timeBgn_A = min(TIMESTAMP_ymd_hms, na.rm = T),
+                   timeEnd_A = max(TIMESTAMP_ymd_hms, na.rm = T),
+                   ## Gas aggregation
+                   mean_CO2_umolpermol = mean(CO2_umolpermol, na.rm = T),
+                   sd_CO2_umolpermol = sd(CO2_umolpermol, na.rm = T),
+                   mean_H2O_mmolpermol = mean(H2O_mmolpermol, na.rm = T),
+                   sd_H2O_mmolpermol = sd(H2O_mmolpermol, na.rm = T),
+                   mean_CH4_nmolpermol = mean(CH4_nmolpermol, na.rm = T),
+                   sd_CH4_nmolpermol = sd(CH4_nmolpermol, na.rm = T),
+                   ## Aggregation of other columns
+                   mean_T_CELL_degC = mean(T_CELL_degC, na.rm = T),
+                   sd_T_CELL_degC = sd(T_CELL_degC, na.rm = T),
+                   mean_PRESS_CELL_kPa = mean(PRESS_CELL_kPa, na.rm = T),
+                   sd_PRESS_CELL_kPa = sd(PRESS_CELL_kPa, na.rm = T),
+                   mean_FLOW_VOLRATE_Lpermin = mean(FLOW_VOLRATE_Lpermin, na.rm = T),
+                   sd_FLOW_VOLRATE_Lpermin = sd(FLOW_VOLRATE_Lpermin, na.rm = T),
+                   mean_FLOW_VOLRATE_IU_mV = mean(FLOW_VOLRATE_IU_mV, na.rm = T),
+                   sd_FLOW_VOLRATE_IU_mV = sd(FLOW_VOLRATE_IU_mV, na.rm = T) ) %>% 
+  dplyr::ungroup() %>% 
+  # Ditch 'halfhour group' now that we've used it
+  dplyr::select(-halfhour_group)
 
-# Generate 30-minute groups
+# Check structure
+dplyr::glimpse(icos_v4)
+## tibble::view(icos_v4)
 
-
-
-
-
-
-## tibble::view(icos_v2)
-
-psych::multi.hist(x = icos_v2$relative_gap)
 
 
 
