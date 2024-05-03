1c530588cfe6e53c3625fc5ed3ce8bec43bffece
njlyon0 <nicholasjlyon@gmail.com>
2024-05-03 10:12:50 -0400

    Fleshed out script skeleton

---
+++ workflows/flow.non.neon.attribute.tables.R
@@ -0,0 +1,28 @@
+## ------------------------------------------------- ##
+# Make Non-NEON Attribute Tables
+## ------------------------------------------------- ##
+# Script author(s): Nick Lyon, Kyle Delwiche
+
+# Purpose:
+## Make attribute tables for non-NEON sites that are consistent with those of NEON sites
+
+## ---------------------------- ##
+# Housekeeping ----
+## ---------------------------- ##
+
+# Load libraries
+# install.packages("librarian")
+librarian::shelf(tidyverse)
+
+# Clear environment
+rm(list = ls())
+
+## ---------------------------- ##
+# Non-NEON Site 1 ----
+## ---------------------------- ##
+
+
+
+
+
+# End ----
