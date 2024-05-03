18ca9ab1dd8bc037abffa1d8c32cc4427817ed3f
njlyon0 <nicholasjlyon@gmail.com>
2024-05-03 10:20:34 -0400

    Drafting script likely workflow without real data

--- workflows/flow.non.neon.attribute.tables.R
+++ workflows/flow.non.neon.attribute.tables.R
@@ -1,5 +1,5 @@
 ## ------------------------------------------------- ##
-# Make Non-NEON Attribute Tables
+          # Make Non-NEON Attribute Tables
 ## ------------------------------------------------- ##
 # Script author(s): Nick Lyon, Kyle Delwiche
 
@@ -7,20 +7,54 @@
 ## Make attribute tables for non-NEON sites that are consistent with those of NEON sites
 
 ## ---------------------------- ##
-# Housekeeping ----
+      # Housekeeping ----
 ## ---------------------------- ##
 
 # Load libraries
 # install.packages("librarian")
-librarian::shelf(tidyverse)
+librarian::shelf(tidyverse, googledrive)
 
 # Clear environment
 rm(list = ls())
+
+# Authorize Google Drive
+googledrive::drive_auth()
+## You'll need to select a pre-authorized email via the Console
+
+# Make a vector of all column names found in an attribute table
+attr_colnames <- c("DistZaxsCnpy", "DistZaxsDisp", "DistZaxsGrndOfst", 
+                   "DistZaxsLvlMeasTow", "DistZaxsTow", "ElevRefeTow", 
+                   "LatTow", "LonTow", "LvlMeasTow", "Pf.AngEnuXaxs", 
+                   "Pf.AngEnuYaxs", "Pf.Ofst", "TimeDiffUtcLt", "TimeTube", 
+                   "ZoneTime", "ZoneUtm", "TowerPosition", "Site")
 
 ## ---------------------------- ##
 # Non-NEON Site 1 ----
 ## ---------------------------- ##
 
+# Read in data
+data <- read.csv(file = file.path())
+
+# Glimpse data
+dplyr::glimpse(data)
+
+# Wrangle data to proper format for attribute table
+attr.df <- data %>% 
+  # Rename desired columns
+  dplyr::rename() %>% 
+  # Keep only those columns
+  dplyr::select() %>% 
+  # Keep only unique rows (i.e., refine to only attribute information)
+  dplyr::distinct()
+
+# Check structure
+dplyr::glimpse(attr.df)
+
+# Save as a .RData object
+
+# Zip it
+
+# Upload to Google Drive
 
 
 
