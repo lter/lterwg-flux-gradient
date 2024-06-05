e92f767d168fabaf2fe03228ada0bc28c8f1f82d
Camilo-Rey <camiloreysanchez@gmail.com>
2024-06-05 14:58:34 -0400

    Added the Wind Profile Method to the calc.eddydiff.aero.R for efficiency. Changes may be revisited based on what we discussed in the meeting of June 5 2024

--- functions/calc.eddydiff.aero.R
+++ functions/calc.eddydiff.aero.R
@@ -9,6 +9,8 @@
 #' 
 calc.eddydiff.aero <- function(sitecode, min9){
   #currently hard coded to calculate for all gas concentrations
+  
+  ### H2O ###
   #grab H2O gas concentration
   H2O <- min9[[which(names(min9) == "H2O")]]
   #remove NAs from data columns used in calculation for AE this includes: z_displ_calc
@@ -21,13 +23,26 @@ calc.eddydiff.aero <- function(sitecode, min9){
   #we need: von karman constant (k), friction velocity (u_star), geometric mean of upper and lower heights (z_g), stability parameter (phih)
   #assuming von karman constant is 0.4
   k = 0.4
-  #why are we using geometric mean instead of regular mean?
-  H2O$GeometricMean_AB <- sqrt(as.numeric(H2O$TowerHeight_A)*as.numeric(H2O$TowerHeight_B))
-  #we want to calculate eddy diffusivity for each height
-  H2O$EddyDiff = (k*as.numeric(H2O$ustar_interp)*as.numeric(H2O$GeometricMean_AB))/as.numeric(H2O$phih)
-  # H2O$EddyDiff_phih = (k*as.numeric(H2O$ustar_interp)*as.numeric(H2O$GeometricMean_AB))/as.numeric(H2O$phih)
-  #try calculating eddy diffusivity with wind shear parameter (phim) instead of heat parameter (phih)
-  # H2O$EddyDiff_phim = (k*as.numeric(H2O$ustar_interp)*as.numeric(H2O$GeometricMean_AB))/as.numeric(H2O$phim)
+
+  # EDDY DIFF AERO: 
+  H2O$EddyDiff = (k*as.numeric(H2O$ustar_interp)*as.numeric(H2O$effective_h))/as.numeric(H2O$phih)
+
+  # EDDY DIFF WP: create column for store wind profile eddy diffusivity with Wind Profiler Method
+  H2O$EddyDiff_WP <- "hold"
+  #TO DO: REFORMAT ubar COLUMNS SO THAT WE CAN SELECT FOR CORRECT ubar USING TowerPosition
+  for(j in 1:dim(H2O)[1]){
+    c.name <- paste0("ubar", as.character(H2O[j,"TowerPosition_A"]))
+    ubar = as.numeric(H2O[j,grep(c.name, names(H2O))])
+    zd = as.numeric(H2O[j,"TowerHeight_A"])
+    zo= as.numeric(H2O[j,"roughLength_calc"])
+    phih=H2O[j,"phih"]
+    
+    H2O[j,"EddyDiff_WP"] <- ((k^2)*ubar*zd)/(log(zd/zo)*phih)
+  }
+  #set EddyDiff as numeric
+  H2O$EddyDiff_WP <- as.numeric(H2O$EddyDiff_WP)
+  
+  ### CO2 ###
   
   #grab CO2 gas concentration
   CO2 <- min9[[which(names(min9) == "CO2")]]
@@ -39,15 +54,28 @@ calc.eddydiff.aero <- function(sitecode, min9){
   #we need: von karman constant (k), friction velocity (u_star), geometric mean of upper and lower heights (z_g), stability parameter (phih)
   #assuming von karman constant is 0.4
   k = 0.4
-  #why are we using geometric mean instead of regular mean?
-  CO2$GeometricMean_AB <- sqrt(as.numeric(CO2$TowerHeight_A)*as.numeric(CO2$TowerHeight_B))
-  #we want to calculate eddy diffusivity for each height
-  CO2$EddyDiff = (k*as.numeric(CO2$ustar_interp)*as.numeric(CO2$GeometricMean_AB))/as.numeric(CO2$phih)
-  # CO2$EddyDiff_phih = (k*as.numeric(CO2$ustar_interp)*as.numeric(CO2$GeometricMean_AB))/as.numeric(CO2$phih)
-  # #try calculating eddy diffusivity with wind shear parameter (phim) instead of heat parameter (phih)
-  # CO2$EddyDiff_phim = (k*as.numeric(CO2$ustar_interp)*as.numeric(CO2$GeometricMean_AB))/as.numeric(CO2$phim)
+
+  # EDDY DIFF AERO:
+  CO2$EddyDiff = (k*as.numeric(CO2$ustar_interp)*as.numeric(CO2$effective_h))/as.numeric(CO2$phih)
   
-  #grab CO2 gas concentration
+  # EDDY DIFF WP: create column for store wind profile eddy diffusivity with Wind Profiler Method
+  CO2$EddyDiff_WP <- "hold"
+  #TO DO: REFORMAT ubar COLUMNS SO THAT WE CAN SELECT FOR CORRECT ubar USING TowerPosition
+  for(j in 1:dim(CO2)[1]){
+    c.name <- paste0("ubar", as.character(CO2[j,"TowerPosition_A"]))
+    ubar = as.numeric(CO2[j,grep(c.name, names(CO2))])
+    zd = as.numeric(CO2[j,"TowerHeight_A"])
+    zo= as.numeric(CO2[j,"roughLength_calc"])
+    phih=CO2[j,"phih"]
+    
+    CO2[j,"EddyDiff_WP"] <- ((k^2)*ubar*zd)/(log(zd/zo)*phih)
+  }
+  #set EddyDiff as numeric
+  CO2$EddyDiff_WP <- as.numeric(CO2$EddyDiff_WP)
+  
+  
+  ### CH4 ###
+  #grab CH4 gas concentration
   CH4 <- min9[[which(names(min9) == "CH4")]]
   #remove NAs
   CH4 <- CH4[complete.cases(CH4[,data.cols]),]
@@ -58,13 +86,24 @@ calc.eddydiff.aero <- function(sitecode, min9){
   #assuming von karman constant is 0.4
   k = 0.4
   #why are we using geometric mean instead of regular mean?
-  CH4$GeometricMean_AB <- sqrt(as.numeric(CH4$TowerHeight_A)*as.numeric(CH4$TowerHeight_B))
-  #we want to calculate eddy diffusivity for each height
-  CH4$EddyDiff = (k*as.numeric(CH4$ustar_interp)*as.numeric(CH4$GeometricMean_AB))/as.numeric(CH4$phih)
-  # CH4$EddyDiff_phih = (k*as.numeric(CH4$ustar_interp)*as.numeric(CH4$GeometricMean_AB))/as.numeric(CH4$phih)
-  # #try calculating eddy diffusivity with wind shear parameter (phim) instead of heat parameter (phih)
-  # CH4$EddyDiff_phim = (k*as.numeric(CH4$ustar_interp)*as.numeric(CH4$GeometricMean_AB))/as.numeric(CH4$phim)
+  CH4$effective_h <- sqrt(as.numeric(CH4$TowerHeight_A)*as.numeric(CH4$TowerHeight_B))
+  #EDDY DIFF AERO
+  CH4$EddyDiff = (k*as.numeric(CH4$ustar_interp)*as.numeric(CH4$effective_h))/as.numeric(CH4$phih)
   
+  # EDDY DIFF WP: create column for store wind profile eddy diffusivity with Wind Profiler Method
+ CH4$EddyDiff_WP <- "hold"
+  #TO DO: REFORMAT ubar COLUMNS SO THAT WE CAN SELECT FOR CORRECT ubar USING TowerPosition
+  for(j in 1:dim(CH4)[1]){
+    c.name <- paste0("ubar", as.character(CH4[j,"TowerPosition_A"]))
+    ubar = as.numeric(CH4[j,grep(c.name, names(CH4))])
+    zd = as.numeric(CH4[j,"TowerHeight_A"])
+    zo= as.numeric(CH4[j,"roughLength_calc"])
+    phih=CH4[j,"phih"]
+    
+   CH4[j,"EddyDiff_WP"] <- ((k^2)*ubar*zd)/(log(zd/zo)*phih)
+  }
+  #set EddyDiff as numeric
+ CH4$EddyDiff_WP <- as.numeric(CH4$EddyDiff_WP)
   
   #add to list
   min9.K.AE.list <- list(H2O = H2O, CO2 = CO2, CH4 = CH4)
