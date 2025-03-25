731872def085c4da9eccdd7ffcf77a7a1b072140
Kyle <kdelwiche@berkeley.edu>
2025-03-25 11:15:44 -0700

    Trying to download files, change column names, re-save them, but it currently doesn't work yet...

--- workflows/flow.non.neon.data.harmonize.ch4.R
+++ workflows/flow.non.neon.data.harmonize.ch4.R
@@ -17,13 +17,14 @@
 # Load needed libraries
 # install.packages("librarian")
 library(data.table)
+library(readr)
 librarian::shelf(tidyverse, googledrive, lter/ltertools)
 
 # Clear environment
 rm(list = ls())
 
 #Site to work on
-siteID <- 'SE-Deg'
+siteID <- 'SE-Sto'
 
 if (siteID == 'US-Uaf'){
   gdrive_path <- "https://drive.google.com/drive/u/0/folders/1AOct-UbwpzkuLMT9EnEspRX_QnX07T4G"
@@ -64,7 +65,7 @@ if (siteID == 'US-Uaf'){
 # Data Download ----
 ## ----------------------------- ##
 
-# Identify desired files WITH CORPUS PARAMETER
+# Identify desired files 
 ch4_files <- googledrive::drive_ls(
   path = googledrive::as_id(gdrive_path),
   ) %>%  
@@ -145,7 +146,7 @@ rm(list = setdiff(ls(), "gdrive_path", "SE-Sto"))
 
 # Identify key
 (key_id <- googledrive::drive_ls(path = googledrive::as_id(gdrive_path)) %>% 
-  dplyr::filter(name == "Methane-Key") )
+  dplyr::filter(name == paste("Methane_key_", siteID, sep='') ))
 
 # Download key
 googledrive::drive_download(file = key_id$id, overwrite = T,
@@ -158,13 +159,38 @@ ch4_key <- read.csv(file = file.path("methane", "methane-key.csv"))
 dplyr::glimpse(ch4_key)
 
 # Harmonize the raw data with that key
-ch4_tidy <- ltertools::harmonize(key = ch4_key, 
-                                 raw_folder = file.path("methane", "raw_methane"),
-                                 data_format = c("csv", "xlsx", "xls"),
-                                 quiet = FALSE)
+# Get list of files in the raw_folder
+all_files <- list.files(path = "methane/raw_methane", full.names = TRUE)
+#rename column names in each file individually, save
+for (file in all_files) {
+  # Read the file
+  if (tools::file_ext(file) == "csv") {
+    data <- read_csv(file)
+  } else {
+    data <- readxl::read_excel(file)
+  }
+  
+  # Get the filename without extension
+  filename <- tools::file_path_sans_ext(basename(file))
+  
+  # Filter the key for this file
+  file_key <- ch4_key %>% filter(source == paste0(filename, ".", tools::file_ext(file)))
+  
+  # Rename columns
+  for (i in 1:nrow(file_key)) {
+    if (!is.na(file_key$tidy_name[i])) {
+      data <- data %>% rename(!!file_key$tidy_name[i] := file_key$raw_name[i])
+    }
+  }
+} 
+  # Save the modified file
+  write_csv(data, file.path(raw_folder, paste0(filename, "_harmonized.csv")))
+}
 
 # Check structure
 dplyr::glimpse(ch4_tidy)
+
+
 
 # Export locally
 write.csv(x = ch4_tidy, na = "", row.names = F,
